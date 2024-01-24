0aeda16c7394e8f4fbf6183e161459215e7bdec1
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-01-23 21:43:43 -0600

    created a displayPlant function + addPlant function

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ const examplePlant = {
 
 const plants = [];
 plants.push(examplePlant);
+
+function displayPlants() {
+  const plantList = document.getElementById("plantList");
+  plantList.innerHTML = "";
+
+  plants.forEach((plant) => {
+    const li = document.createElement("li");
+    li.innerHTML = `Name: ${plant.name}, Species: ${plant.species}, Water Schedule: ${plant.water}`;
+    plantList.appendChild(li);
+  });
+}
+displayPlants();
+
+function addPlants(name, species, water) {
+  const newPlant = { name, species, water };
+  plants.push(newPlant);
+}
